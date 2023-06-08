a7c0f36be90380c7114c51381897063b2d27f288
baonguyen2310 <lanchinhaicon@gmail.com>
2023-06-08 17:22:14 +0700

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,22 @@
-const NavBar = () => {
-    return 
-}
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-export { NavBar }
+const NavBar = () => {
+  return (
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+            <Navbar.Brand href="#" className="ms-3">Bao Nguyen</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" data-bs-toggle="collapse" data-bs-target="#navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav className="d-flex justify-content-end me-3 ms-3" style={{ width: "100%" }}>
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                    <Nav.Link href="#skills">Skills</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export { NavBar };
